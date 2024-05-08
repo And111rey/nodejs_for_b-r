907d0bc53e819cbf1d23aaf2e840d91e70d46ca8
And111rey <samu111ray@gmail.com>
2024-05-08 17:07:10 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+
+const PORT = 5000
+
+const app = express()
+app.use(express.json())
+
+app.get("/", (req, res) =>{
+    console.log(req.query)
+    res.status(200).json('server working....')
+})
+
+
+
+app.post("/", (req, res) =>{
+    console.log(req.body)
+    res.status(200).json('server working....')
+})
+app.listen(PORT, ()=> console.log(`server on ${PORT}`))
